ff8d44a8b9ec6dc6d6cc2f9b732c43a98ea5af82
Jalaj <jalajdorai100@gmail.com>
2020-04-11 10:18:01 +0530

    Updates

---
+++ precache-manifest.f2b73d8f84d87039c06f95f1d1d56303.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f7d4c1f3ed06a53c2bc935bea17f5f",
+    "url": "/jalajdorai/corona-tracker/index.html"
+  },
+  {
+    "revision": "885d8af7323390e2a13f",
+    "url": "/jalajdorai/corona-tracker/static/css/main.efc4b8b4.chunk.css"
+  },
+  {
+    "revision": "f2883c7f03e8dc5e6e5d",
+    "url": "/jalajdorai/corona-tracker/static/js/2.88516caf.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/jalajdorai/corona-tracker/static/js/2.88516caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "885d8af7323390e2a13f",
+    "url": "/jalajdorai/corona-tracker/static/js/main.86b82c6d.chunk.js"
+  },
+  {
+    "revision": "329b2756c434e16097b7",
+    "url": "/jalajdorai/corona-tracker/static/js/runtime-main.0bbed53e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/jalajdorai/corona-tracker/static/media/image.d7265326.png"
+  }
+]);
